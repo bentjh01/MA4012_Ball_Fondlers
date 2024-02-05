86cfbee2a3e2737bc5d55bff490dbf5bf8a68c8f
bentjh01 <benjamintehjh01@gmail.com>
2024-02-05 10:36:39 +0800

    added to agenda

--- ROBOTC_ws/main.c
+++ ROBOTC_ws/main.c
@@ -1,9 +1,41 @@
 
+task search_ball()
+{
 
+
+
+}
+
+task goto_ball()
+{ 
+
+
+
+}
+
+task collect_ball()
+{
+
+
+}
+
+
+task deliver_ball()
+{
+
+}
+
+
+task edge_detection()
+{
+
+}
 
 task main()
 {
 
-
+  while (1){
+    
+  }
 
 }
