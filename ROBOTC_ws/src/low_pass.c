79ed07ae5491ce012eef37f22defe34f53f16788
bentjh01 <benjamintehjh01@gmail.com>
2024-02-05 11:58:04 +0800

    low pass typo

--- ROBOTC_ws/src/low_pass.c
+++ ROBOTC_ws/src/low_pass.c
@@ -7,5 +7,5 @@ void low_pass_filter_init(LowPassFilter* filter, float alpha, float initial_outp
 
 float lowPassFilter(LowPassFilter* filter, float input) {
     filter->prev_output = (filter->alpha * input) + ((1 - filter->alpha) * filter->prev_output);
-    return filter->prevOutput;
+    return filter->prev_output;
 }
