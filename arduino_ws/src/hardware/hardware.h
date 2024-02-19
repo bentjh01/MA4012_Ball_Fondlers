102afd1ecc015f5644d44187dbbb97cc3473161c
bentjh01 <benjamintehjh01@gmail.com>
2024-02-20 00:45:35 +0800

    init arduino_ws

---
+++ arduino_ws/src/hardware/hardware.h
@@ -0,0 +1,10 @@
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+#include <Arduino.h>
+
+constexpr int led_pin = 13;
+
+void init_hardware(void);
+
+#endif // HARDWARE_H
---
+++ arduino_ws/src/main.cpp
@@ -0,0 +1,9 @@
+#include "main.h"
+
+void init_main(void){
+	init_hw();
+}
+
+void main_run(void){
+	blink_led(1000);
+}
