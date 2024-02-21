c421127b4f7cdf8dae498e184398d51203dfbf00
bentjh01 <benjamintehjh01@gmail.com>
2024-02-22 00:21:52 +0800

    description

--- arduino_ws/src/blink_led/blink_led.h
+++ arduino_ws/src/blink_led/blink_led.h
@@ -1,8 +1,10 @@
-#ifndef BLINK_LED_H
+#ifndef BLINK_LED_H // prevents duplicate definition of the same library
 #define BLINK_LED_H
-
-#include <Arduino.h>
-#include "../hardware/hardware.h"
+// <> means it's a global library i.e. built-in libraries such as Wire.h, SPI.h, etc.
+#include <Arduino.h> 
+// "" means it's a local library i.e. libraries that we created. local libraries are relative.
+// .. means go up one directory.
+#include "../hardware/hardware.h" 
 
 void blink_led(int duration);
 
