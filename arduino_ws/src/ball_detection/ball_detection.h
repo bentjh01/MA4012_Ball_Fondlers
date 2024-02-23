bca314886b1fbe3365bbbfe3d6503e183b09126f
ShaDowFrost2417 <100888793+ShaDowFrost2417@users.noreply.github.com>
2024-02-24 01:52:56 +0800

    Update ball_detection.h

--- arduino_ws/src/ball_detection/ball_detection.h
+++ arduino_ws/src/ball_detection/ball_detection.h
@@ -1,10 +1,22 @@
 #ifndef BALL_DETECTION_H // prevents duplicate definition of the same library
 #define BALL_DETECTION_H
-// <> means it's a global library i.e. built-in libraries such as Wire.h, SPI.h, etc.
-#include <Arduino.h> 
+
+#include <Arduino.h>
+#include <math.h> //will be used later to convert analogRead() to distance in cm
+
+extern float search_timer;
+extern int l_long_pin;
+extern int r_long_pin;
+extern int c_long_pin;
+extern int c_short_pin;
+
+float sense_long_distance(int pin_num);
+float sense_short_distance(int pin_num);
+int read_long_status(int pin_num);
+int read_short_status(int pin_num);
 
 void change_search_position();
-void detect_ball(startup_phase);
-void go_to_ball();
+int detect_ball(bool startup_phase);
+int go_to_detection(int status, bool startup_phase);
 
-#endif // BLINK_LED_H
+#endif // BALL_DETECTION_H
