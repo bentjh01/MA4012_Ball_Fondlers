491bf3efccf0787c14a89a8f25ed977d2439277e
bentjh01 <benjamintehjh01@gmail.com>
2024-02-25 08:04:53 +0800

    init const for wheel and track

--- arduino_ws/src/config.h
+++ arduino_ws/src/config.h
@@ -2,3 +2,11 @@
 
 // May need to add for each individual sensor
 #define LINE_SENSOR_THRESHOLD 512
+
+// Direction of delivery area
+#define DELIVERY_AREA_DIRECTION 50 // [degrees]
+
+
+// CONSTANTS
+constexpr float wheel_diameter = 0.05; //m
+constexpr float robot_track = 0.3; //m
