e00a7cda61f0be5413834154c2d7ddb9990c4133
ShaDowFrost2417 <100888793+ShaDowFrost2417@users.noreply.github.com>
2024-03-06 21:39:10 +0800

    Update ball_detection.h
    Moved constant variable definitions from .cpp file to here.
    
    Made changes according to the commit in the .cpp file (making the code clearer and finishing the change_search_position by moving forward 80 cm)

--- arduino_ws/src/ball_detection/ball_detection.h
+++ arduino_ws/src/ball_detection/ball_detection.h
@@ -2,22 +2,36 @@
 #define BALL_DETECTION_H
 
 #include <Arduino.h>
-#include <math.h> //will be used later to convert analogRead() to distance in cm
+#include <math.h>
+#include "motors.h"
+#include "hardware.h"
+
+#define LONG_DIST_LOWER_THRESHOLD_CM 25      //lower threshold of long dist sensor 25 cm
+#define LONG_DIST_UPPER_THRESHOLD_CM 45      //upper threshold of long dist sensor 45 cm
+#define SHORT_DIST_THRESHOLD_CM 20 //upper threshold of short dist sensor 20 cm (example only). Make this <= LOWER_THRESHOLD_CM
+#define VOLTAGE_SCALE (5.0/1023.0)
+#define RAD_TO_DEG (180.0 / PI)
+#define BALL_TO_ROBOT_CENTER 15.0 //approx distance between the object detected by the left/right sensor wrt the center of ball collection mechanism in an axis perpendicular to robot direction. 15 cm is example only.
+#define TOLERANCE_CM 5.0
+#define FRACTIONAL_CLOSENESS_TO_BALL 0.80
 
 extern float search_timer;
-extern int l_long_pin;
-extern int r_long_pin;
-extern int c_long_pin;
-extern int c_short_pin;
-extern robot_moving_timeout;
+extern robot_moving_timeout_milisecond;
 
-float sense_long_distance(int pin_num);
-float sense_short_distance(int pin_num);
-int read_long_status(int pin_num);
-int read_short_status(int pin_num);
+typedef struct {
+	float linear_velocity;
+	float angular_velocity;
+  float time_setting_milisecond;
+} circular_path;
 
-void change_search_position();
-int detect_ball(bool startup_phase);
+float read_long_sensor_distance(int pin_num);
+float read_short_sensor_distance(int pin_num);
+int read_digitized_long_sensor_distance(int pin_num);
+int read_digitized_short_sensor_distance(int pin_num);
+int check_status_from_left_right_sensor(int long_distance_sensor_FL_status, int long_distance_sensor_FR_status);
+circular_path calculate_translation_angular_speed(int pin_num, float angular_velocity_DegPerSec);
+int change_search_position(bool startup_phase);
+int scan(bool startup_phase);
 int go_to_detection(int status, bool startup_phase);
 
 #endif // BALL_DETECTION_H
