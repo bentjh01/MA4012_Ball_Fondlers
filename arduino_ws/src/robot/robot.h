536706316c2094f2a040c490de901593fa01c720
bentjh01 <benjamintehjh01@gmail.com>
2024-03-07 00:36:48 +0800

    kalman filter on robot pose

---
+++ arduino_ws/src/robot/robot.h
@@ -0,0 +1,27 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#include <math.h>
+#include "../motors/motors.h"
+
+#define ROBOT_TRACK 0.171674 // [m]
+
+#define ENCODER_FILTER_GAIN 0.5
+#define MAGNETO_FILTER_GAIN 0.5
+
+typedef struct {
+	float x;
+	float y;
+	float yaw;
+	float linear_velocity;
+	float angular_velocity;
+} pose;
+
+typedef struct {
+    float linear_velocity;
+    float angular_velocity;
+} twist;
+
+extern pose robot_pose;
+
+#endif // ROBOT_H
