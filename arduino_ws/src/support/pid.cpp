579de59f58195a56cd1570ea3ab76ed6bcea2a62
bentjh01 <benjamintehjh01@gmail.com>
2024-03-11 14:56:44 +0800

    pid update to functional

--- arduino_ws/src/support/pid.cpp
+++ arduino_ws/src/support/pid.cpp
@@ -1,19 +1,21 @@
 // PID controller implementation
 #include "pid.h"
 
-void pid_init(PIDController* pid, float Kp, float Ki, float Kd, float setpoint) {
-    pid->Kp = Kp;
-    pid->Ki = Ki;
-    pid->Kd = Kd;
+PIDController pid_init(float Kp, float Ki, float Kd, float setpoint) {
+    PIDController pid;
+    pid.Kp = Kp;
+    pid.Ki = Ki;
+    pid.Kd = Kd;
     pid->setpoint = setpoint;
     pid->error_sum = 0.0;
     pid->prev_error = 0.0;
     pid->prev_time = millis()/1000;
+    return PIDController;
 }
 
-float pid_update(PIDController* pid, float input) {
+PID pid_update(PIDController pid, float feedback) {
     float dt = millis()/1000 - pid->prev_time;
-    float error = pid->setpoint - input;
+    float error = pid->setpoint - feedback; 
     pid->error_sum += error * dt;
     float derivative = (error - pid->prev_error) / dt;
     float output = pid->Kp * error + pid->Ki * pid->error_sum + pid->Kd * derivative;
@@ -22,6 +24,11 @@ float pid_update(PIDController* pid, float input) {
     return output;
 }
 
-void pid_setpoint(PIDController* pid, float setpoint) {
-    pid->setpoint = setpoint;
+PIDController deliver = pid_init(1.0, 0.0, 0.0, 0.0, 0.0);
+
+void loop(){
+    deliver.setpoint = 0.0;
+    deliver = pid_update(deliver, 0.0);
+    output = pid.output;
+    // motor_request(output, output);
 }
