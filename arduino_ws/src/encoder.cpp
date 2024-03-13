050c45a4b7ba002494b0da4cde2de8a6aa54fd56
Unizz20 <157607179+Unizz20@users.noreply.github.com>
2024-03-13 14:45:36 +0800

    Create encoder.cpp

---
+++ arduino_ws/src/encoder.cpp
@@ -0,0 +1,14 @@
+#pragma config(Sensor, dgtl3,  right_encoder,  sensorQuadEncoder)
+#pragma config(Sensor, dgtl7,  left_encoder,   sensorQuadEncoder)
+
+
+task encoder(){
+	// Clear Encoders
+	SensorValue[right_encoder] = 0;
+	SensorValue[left_encoder] = 0;
+	
+	// Covert to Degrees/Radians (-647 to 647)
+	RE_deg = ((SensorValue[right_encoder])*(360/647));
+	RE_rad = (RE_deg * (pi/180))
+	
+}
