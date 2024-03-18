0bf605952d09c90643a67cb76eefc9e4263db366
Unizz20 <157607179+Unizz20@users.noreply.github.com>
2024-03-18 15:23:03 +0800

    Update encoder.cpp

--- arduino_ws/src/encoder.cpp
+++ arduino_ws/src/encoder.cpp
@@ -9,10 +9,10 @@ task encoder(){
 	
 	// Covert to Degrees/Radians (-647 to 647) -> 647ticks/rev
 	int RE_deg_right = ((SensorValue[right_encoder])*(360/647));
-	int RE_rad_right = (RE_deg_right * (pi/180));
+	int RE_rad_right = (RE_deg_right * (3.1415/180));
 
 	// Covert to Degrees/Radians (-647 to 647) -> 647ticks/rev
 	int RE_deg_left = ((SensorValue[left_encoder])*(360/647));
-	int RE_rad_left = (RE_deg_left * (pi/180));
+	int RE_rad_left = (RE_deg_left * (3.1415/180));
 	
 }
