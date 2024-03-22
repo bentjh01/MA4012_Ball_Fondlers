bfdbb0efa1c0751c225cee4554029823e26a04a9
Unizz20 <157607179+Unizz20@users.noreply.github.com>
2024-03-22 21:27:24 +0800

    Create Reset_Start_Switch.cpp

---
+++ ROBOTC_ws/Reset_Start_Switch.cpp
@@ -0,0 +1,34 @@
+#pragma config(Sensor, dgtl1,  Reset_Start_Switch, sensorDigitalIn)
+#pragma config(Motor,  port6,           motor_R,       tmotorVex393_MC29, PIDControl, encoderPort, dgtl4)
+#pragma config(Motor,  port7,           motor_L,       tmotorVex393_MC29, PIDControl, reversed, encoderPort, dgtl2)
+
+
+void init_robot(void){
+	robot_x = 0;
+  robot_y = 0;
+  robot_yaw = 0;
+  robot_linX = 0;
+  robot_angZ = 0;
+  robot_rpmR = 0;
+  robot_rpmL = 0;
+  resetMotorEncoder(motor_R);
+	resetMotorEncoder(motor_L);
+}
+
+void center_start(){
+	motor[motor_R] = 127;
+	motor[motor_L] = 127;
+	delay(1000);
+	motor[motor_R] = 0;
+	motor[motor_L] = 0;
+}
+
+void reset_start()
+{
+	while(true){
+		if(SensorValue(Reset_Start_Switch) == 1){
+			init_robot();
+			center_start();			
+		}
+	}
+}
