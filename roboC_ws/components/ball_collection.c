118ec96caadac54031793e00546839edbf9b5df6
Unizz20 <157607179+Unizz20@users.noreply.github.com>
2024-04-02 19:27:14 +0800

    Update and rename ball_colleciton.c to ball_collection.c

--- roboC_ws/components/ball_collection.c
+++ roboC_ws/components/ball_collection.c
@@ -3,8 +3,8 @@ void collect_ball(){
 	// Start Ball Collection
     // Check if status = collection, and, gate is open
 	if (task_Status == 4 && limit_3 == 0){
-        if (short_dist_sensor > 5 and long_dist_sensor_mid < 7){
-            robot_servo_move;
+		if (short_dist_sensor > 5 and long_dist_sensor_mid < 7){
+            		robot_servo_move;
         }
 
         // Check if ball in the chamber
