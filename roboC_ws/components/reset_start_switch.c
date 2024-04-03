afbb825145a03858cebcd4216375b5b5b509ada8
Unizz20 <157607179+Unizz20@users.noreply.github.com>
2024-04-03 15:46:36 +0800

    update

--- roboC_ws/components/reset_start_switch.c
+++ roboC_ws/components/reset_start_switch.c
@@ -1,30 +1,27 @@
-void init_robot(void){
-	robot_x = 0;
-    robot_y = 0;
-    robot_yaw = 0;
-    robot_linX = 0;
-    robot_angZ = 0;
-    robot_rpmR = 0;
-    robot_rpmL = 0;
-    resetMotorEncoder(motor_R);
-	resetMotorEncoder(motor_L);
-}
+
+static float reset_start_linX = 0.0;
+static float reset_start_angZ = 0.0;
+static float reset_start_servo = 0.0;
 
 // Move to center of arena
-void move_to_center(){
-	if (robot_x < 120){ // arena length/2 = 2.4m/2
-		robot_move_closed(127, 0);
+int move_to_center(){
+	if (robot_x < 1.2){ // arena length/2 = 2.4m/2
+		reset_start_linX = MAX_SPEED;
+		reset_start_angZ = 0;
+		reset_start_servo = SERVO_HOME_POSITION;
 	}
 	else{
-		robot_move_closed(0,0);
+		reset_start_linX = 0;
+		reset_start_angZ = 0;
+		reset_start_servo = SERVO_HOME_POSITION;
 		int center_state = 1; // Robot reached the center of arena
 	}
 }
 	
-void center_start(){
+int center_start(){
 	// Begin Search upon reaching the center
 	if (center_state == 1){
-		task_status = 2; // Search status
+		return SEARCH;
 	}
 	else {
 		move_to_center();
@@ -33,19 +30,32 @@ void center_start(){
 
 void reset_start()
 {
-	while(true){
-		if(SensorValue(Reset_Start_Switch) == 0){
-			// Initialise all conditions of the robot
-			init_robot();
-			task_status = 0; // Home status
+	if(SensorValue(Reset_Start_Switch) == 0){
+		// Initialise all conditions of the robot
+		init_robot();
+		return HOME;
+		
+		if (SensorValue(Reset_Start_Switch) == 1){
+			// Move robot to center of arena
+			move_to_center();
 			
-			if (SensorValue(Reset_Start_Switch) == 1){
-				// Move robot to center of arena
-				move_to_center();
-				
-				// Start Search
-				center_start();			
+			// Start Search
+			center_start();			
 		}
 	}
+}
+
+
+
+float get_collect_linX(){
+    return reset_start_linX;
+}
+
+float get_collect_angZ(){
+    return reset_start_angZ;
+}
+
+float get_collect_servo(){
+    return reset_start_servo;
 }
 
