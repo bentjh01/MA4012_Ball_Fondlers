fd6053d596aa73c9c35ac9b61bbe26a62dae9dae
ShaDowFrost2417 <100888793+ShaDowFrost2417@users.noreply.github.com>
2024-04-09 16:36:38 +0000

    made search and goto task clearer

--- roboC_ws/robotTasks/goto_task.c
+++ roboC_ws/robotTasks/goto_task.c
@@ -15,7 +15,6 @@
 static int ball_location;
 static int prev_ball_location;
 static float goto_initial_yaw;
-static float goto_current_yaw;
 static float goto_target_yaw;
 static int goto_correction_mode = 0;
 static int goto_different_sign;
@@ -25,17 +24,6 @@ static int goto_startup_phase = 1;
 static float goto_angular_difference;
 static int activate_goto_sweep = 0;
 static int goto_sweep_counter = 0;
-
-// int goto_opponent_detected(float short_sensor_dist){
-//     //may need to move this to components into sensors.c
-//     if(short_sensor_dist <= OPP_CLOSENESS_THRESHOLD){
-//         //Theres an opp robot close in front
-//         return OPP_DETECTED;
-//     }
-//     else{
-//         return 0;
-//     }
-// }
 
 int get_ball_location(float left_sensor_dist, float right_sensor_dist, float mid_sensor_dist, int opp_detected){
     // Returns the location of the detected ball
@@ -70,30 +58,52 @@ float calculate_goto_target_yaw(float goto_initial_yaw, float yaw){
     }
 }
 
+void go_to_target_yaw(float target_yaw, float yaw, int move_forward){
+    goto_angular_difference = target_yaw - yaw;
+
+    if(goto_angular_difference <= -180) {
+    goto_angular_difference = goto_angular_difference+360;
+    }
+    else if (goto_angular_difference > 180){
+        goto_angular_difference = goto_angular_difference-360;
+    }
+
+    goto_linX = move_forward * 0.2;
+    goto_angZ = GOTO_ALIGN_BALL_GAIN * goto_angular_difference;
+    return;
+}
+
+void goto_sweep(){
+    if(goto_angZ < 0.0){
+        if(goto_sweep_counter < round(GOTO_SWEEP_TIME/DT_MAIN)){
+            goto_sweep_counter += 1;
+        }
+        else{
+            goto_linX = 0.0;
+            goto_angZ = 15.0;
+        }
+    }
+    else{
+        goto_sweep_counter += 1;
+    }
+    return;
+}
+
 int goto_task(float x, float y, float yaw, float left_sensor_dist, float right_sensor_dist, float mid_sensor_dist, int opp_detected, float ball_yaw){
     //turn to ball_yaw when first started
     if(goto_startup_phase){
-    	goto_angular_difference = ball_yaw - yaw;
-
-    	if(goto_angular_difference <= -180) {
-        goto_angular_difference = goto_angular_difference+360;
-        }
-        else if (goto_angular_difference > 180){
-            goto_angular_difference = goto_angular_difference-360;
-        }
-
-        goto_linX = 0.0;
-        goto_angZ = GOTO_ALIGN_BALL_GAIN * goto_angular_difference;
+    	go_to_target_yaw(ball_yaw, yaw, 0);
 	    
+        //stop go_to_ball_yaw() when the robot has either faced towards ball_yaw or the ball is already detected
         ball_location = get_ball_location(left_sensor_dist, right_sensor_dist, mid_sensor_dist, opp_detected);     //get latest ball location
-	    
 	    if(fabs(yaw-ball_yaw) <= YAW_TOLERANCE || ball_location != 0){
 	    	goto_startup_phase = 0;
 		    goto_linX = 0.0;
         	goto_angZ = 0.0;
 	    	
-	    	//check if sweep is needed
+	    	//check if sweep is needed (needed when already reached ball_yaw, but no detections are ever found)
 	    	if(ball_location == 0){
+                //initialize sweeping movement
 	    		goto_sweep_counter = 0;
 	    		activate_goto_sweep = 1;
 	    		goto_linX = 0.0;
@@ -105,9 +115,8 @@ int goto_task(float x, float y, float yaw, float left_sensor_dist, float right_s
 
     ball_location = get_ball_location(left_sensor_dist, right_sensor_dist, mid_sensor_dist, opp_detected);     //get latest ball location
 
-    //if no detection even after going to the supposed ball_yaw
+    //sweep until something is detected
     if(activate_goto_sweep){
-    	//sweep until something is detected
     	//check for ball
     	if(ball_location != 0){
     		goto_linX = 0.0;
@@ -117,36 +126,22 @@ int goto_task(float x, float y, float yaw, float left_sensor_dist, float right_s
     	}
     	
     	//sweep
-    	if(goto_angZ < 0.0){
-    		if(goto_sweep_counter < round(GOTO_SWEEP_TIME/DT_MAIN)){
-    			goto_sweep_counter += 1;
-    		}
-    		else{
-    			goto_linX = 0.0;
-        		goto_angZ = 15.0;
-    		}
-    	}
-    	else{
-    		if(goto_sweep_counter < round(3.0*GOTO_SWEEP_TIME/DT_MAIN)){
-    			goto_sweep_counter += 1;
-    		}
-    		else{
-    			goto_linX = 0.0;
-        		goto_angZ = 0.0;
-        		activate_goto_sweep = 0;
-        		return SEARCH;
-    		}
-    	}
+    	goto_sweep()
+        //return to search if after sweeping nothing is found
+        if(goto_sweep_counter > round(3.0*GOTO_SWEEP_TIME/DT_MAIN)){
+            goto_linX = 0.0;
+            goto_angZ = 0.0;
+            activate_goto_sweep = 0;
+            return SEARCH;
+        }
     	return GOTO;
     }
 
     //correction mode activated, go to the middle yaw
-    if(goto_correction_mode != 0){
-        //rotate depending on correction mode
-        goto_linX = 0.20;
-        goto_angZ = goto_correction_mode*15.0;
+    if(goto_correction_mode){
+        go_to_target_yaw(goto_target_yaw, yaw, 1);
 
-        //When approximately facing towards the ball
+        //When approximately facing towards the ball or ball is detected by middle sensor
         if(fabs(yaw-goto_target_yaw) < YAW_TOLERANCE || (ball_location == FRONT && !opp_detected)){
             //move forward
             goto_linX = 0.20;
@@ -156,22 +151,20 @@ int goto_task(float x, float y, float yaw, float left_sensor_dist, float right_s
         return GOTO;
     }
 
-    //Check if there is new detection that is different from the previous (Cos if e.g. detect left, then left again, can be 2 different balls)
+    //Check if there is new detection that is different from the previous
     if(ball_location != 0 && prev_ball_location != ball_location){
         prev_ball_location = ball_location; //save prev ball location
     }
 
-    if(ball_location == FRONT && opp_detected){
-        //Opponent, not ball
+    //Do different things when detecting a ball in different positions
+    if(ball_location == FRONT && opp_detected){ //Opponent, not ball
         //stop movement
         goto_linX = 0.0;
         goto_angZ = 0.0;
         goto_startup_phase = 1;
         return SEARCH;
-        //return GOTO;
     }
-    else if(ball_location == FRONT && !opp_detected){
-        //Ball in front, check if need to move forward to get closer to the ball
+    else if(ball_location == FRONT && !opp_detected){ //Ball in front, check if need to move forward to get closer to the ball
         if(mid_sensor_dist > READY_TO_COLLECT_THRESHOLD){
             //move forward
             goto_linX = 0.20;
@@ -186,35 +179,33 @@ int goto_task(float x, float y, float yaw, float left_sensor_dist, float right_s
             return COLLECT;
         }
     }
-    else if(ball_location == LEFT){
-        //Ball on the left
+    else if(ball_location == LEFT){ //Ball on the left
         //if ball previously detected on the right, now detected on to the left. Meaning the ball is too far for the middle sensor to pick up.
         if(prev_ball_location == RIGHT){
-            goto_correction_mode = 1;                                           //activate correction mode
-            goto_target_yaw = calculate_goto_target_yaw(goto_initial_yaw, yaw); //calculate target yaw (approximate where the ball is)
+            //activate correction mode
+            goto_correction_mode = 1;                                           
+            goto_target_yaw = calculate_goto_target_yaw(goto_initial_yaw, yaw);
             return GOTO;
         }
-        else{
-	    //ball detected on the left for the first time
+        else{ //ball detected on the left for the first time
+            //record yaw and rotate CCW
             goto_initial_yaw = yaw;
-            //rotate CCW
             goto_linX = 0.20;
             goto_angZ = 15.0;
             return GOTO;
         }
     }
-    else if(ball_location == RIGHT){
-        //Ball on the right
+    else if(ball_location == RIGHT){ //Ball on the right
         //if ball previously detected on the left, now detected on to the right. Meaning the ball is too far for the middle sensor to pick up.
         if(prev_ball_location == LEFT){
-            goto_correction_mode = -1;                                           //activate correction mode
-            goto_target_yaw = calculate_goto_target_yaw(goto_initial_yaw, yaw); //calculate target yaw (approximate where the ball is)
+            //activate correction mode
+            goto_correction_mode = 1;                                           
+            goto_target_yaw = calculate_goto_target_yaw(goto_initial_yaw, yaw);
 	        return GOTO;
         }
-        else{
-	    //ball detected on the right for the first time
+        else{ //ball detected on the right for the first time
+            //record yaw and rotate CW
             goto_initial_yaw = yaw;
-            //rotate CW
             goto_linX = 0.20;
             goto_angZ = -15.0;
             return GOTO;
--- roboC_ws/robotTasks/search_task.c
+++ roboC_ws/robotTasks/search_task.c
@@ -24,7 +24,39 @@ static float search_angular_difference;
 //MOVE OPP DETECTION TO
 //MAIN CODE GET SENSOR READING IN CM
 
+void search_scan_rotation(){
+  //start reducing speed after done with half of the 360 degree rotation (180 degree)
+  if((fabs(search_initial_yaw)+fabs(yaw)) >= 175.0 && (fabs(search_initial_yaw)+fabs(yaw)) <= 185.00){
+    search_reduce = 1;
+  }
 
+  if(search_reduce == 0){
+    //before 180 degree, rotate ccw constant speed
+    search_linX = 0.0;
+    search_angZ = 60.0;
+  }
+  else{
+    search_angular_difference = search_initial_yaw-yaw
+
+    //Calculate angular difference (in vector) for +/- pi range
+    if(search_angular_difference <= -180) {
+      search_angular_difference = search_angular_difference+360;
+    }
+    else if (search_angular_difference > 180){
+      //technically this should never occur since we always want to rotate ccw. But it is here just in case.
+      search_angular_difference = search_angular_difference-360;
+    }
+
+    search_linX = 0.0;
+    search_angZ = fabs(search_angular_difference)/180.0*60.0;
+
+    //limit lowest speed to 30 degrees/s
+    if(search_angZ < 30.0){
+      search_angZ = 30.0;
+    }
+  }
+  return;
+}
 
 int search_task(float x, float y, float yaw, float left_sensor_dist, float right_sensor_dist, float mid_sensor_dist, int opp_detected, float search_current_rpmL, float search_current_rpmR){
   //Scanning
@@ -38,44 +70,11 @@ int search_task(float x, float y, float yaw, float left_sensor_dist, float right
       	return SEARCH;
   		}
 
-      search_startup_phase = 0;         //set search_startup_phase
-      search_reduce = 0;		//reset search_proportional_offset
-
-      search_initial_yaw = yaw; //initialize yaw
-
-      if(search_initial_yaw < -180.0){
-      	search_initial_yaw = search_initial_yaw + 360;
-      }
-
-      search_counter = 0;              //reset counter
-    }
-
-    if((fabs(search_initial_yaw)+fabs(yaw)) >= 175.0 && (fabs(search_initial_yaw)+fabs(yaw)) <= 185.00){
-    	search_reduce = 1;
-    }
-
-    if(search_reduce == 0){
-    	//rotate ccw, try find detection
-      search_linX = 0.0;
-      search_angZ = 60.0;
-    }
-    else{
-      search_angular_difference = search_initial_yaw-yaw
-
-      if(search_angular_difference <= -180) {
-        search_angular_difference = search_angular_difference+360;
-      }
-      else if (search_angular_difference > 180){
-        //technically this should never occur since we always want to rotate ccw. But it is here just in case.
-        search_angular_difference = search_angular_difference-360;
-      }
-
-      search_linX = 0.0;
-      search_angZ = fabs(search_angular_difference)/180.0*60.0;
-
-      //if(search_angZ < 60.0){
-      	//search_angZ = 60.0;
-      //}
+      //initialization and end startup
+      search_startup_phase = 0;
+      search_reduce = 0;
+      search_initial_yaw = yaw;
+      search_counter = 0;
     }
 
     //check for ball
@@ -85,11 +84,12 @@ int search_task(float x, float y, float yaw, float left_sensor_dist, float right
       search_angZ = 0.0;
       ball_yaw = yaw;
 
-      search_startup_phase = 1;                               //reset search_startup_phase
+      search_startup_phase = 1; //reset search_startup_phase
       return GOTO;
-    	//return SEARCH;
     }
 
+    //execute rotation
+    search_scan_rotation();
     search_counter += 1; //increment counter
 
     //check if 360 deg rotation achieved
@@ -98,44 +98,39 @@ int search_task(float x, float y, float yaw, float left_sensor_dist, float right
       search_linX = 0.0;
       search_angZ = 0.0;
 
-      search_startup_phase = 1;                               //reset search_startup_phase
-      changing_search_position = 1;                           //initiate change search position
+      search_startup_phase = 1;     //reset search_startup_phase
+      changing_search_position = 1; //initiate change search position
     }
-
     return SEARCH;
   }
-  //Changing search position
-  else{
+  else{ //Changing search position == 1
     if(search_startup_phase){
+      //reset any movement
     	if(fabs(search_current_rpmL) > 0.0001 || fabs(search_current_rpmR) >= 0.0001){
   			search_linX = 0.0;
       	search_angZ = 0.0;
       	return SEARCH
   		}
 
-      search_startup_phase = 0;                               //set search_startup_phase
-      //move forward
+      //initialization and end startup
+      search_startup_phase = 0;
       search_linX = 0.20;
       search_angZ = 0.0;
-
-      //initialize position
       search_initial_x = x;
       search_initial_y = y;
     }
 
-    change_position_traveled_distance = sqrt(pow(x-search_initial_x, 2) + pow(y-search_initial_y, 2);
+    change_position_traveled_distance = sqrt(pow(x-search_initial_x, 2) + pow(y-search_initial_y, 2));
 
-    //check for ball and opp
+    //check for ball
     if (detect_ball(left_sensor_dist,right_sensor_dist, mid_sensor_dist, opp_detected) == 1){
       //stop movement
       ball_yaw = yaw;
       search_linX = 0.0;
       search_angZ = 0.0;
 
-      search_startup_phase = 1;
-
-      //reset search_startup_phase
-      changing_search_position = 0;                           //terminate change_search_position
+      search_startup_phase = 1;     //reset search_startup_phase
+      changing_search_position = 0; //terminate change_search_position
       return GOTO;
     }
 
