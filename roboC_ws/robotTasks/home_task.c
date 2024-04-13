73eddd9dd14c3805c069fcde8310204176150864
Benjamin Teh <benjamintehjh01@gmail.com>
2024-04-14 01:46:11 +0800

    home timeout added

--- roboC_ws/robotTasks/home_task.c
+++ roboC_ws/robotTasks/home_task.c
@@ -4,8 +4,8 @@ static float home_linX = 0.0;
 static float home_angZ = 0.0;
 static float home_servo = 0.0;
 
-int home_task(float x, float y, float yaw, float arm_position, float sensor_mid, float sensor_top, int opp_detected, float sensor_left, float sensor_right){
-    static int home_startup_phase = 1;
+int home_task(float x, float y, float yaw, float arm_position, float sensor_mid, float sensor_top, float sensor_left, float sensor_right){
+    static int home_startup = 1;
     static int home_initial_x = 0.0;
     static float home_count = 0.0;
     home_servo = 0.0;
@@ -13,27 +13,27 @@ int home_task(float x, float y, float yaw, float arm_position, float sensor_mid,
     home_angZ = -yaw;
     home_angZ = 0.0;
     
-    if(home_startup_phase){
+    if(home_startup == 1){
         home_count = 0.0;
         home_linX = 0.0;
         home_angZ = 0.0;
         home_servo = 0.0;
         
     	home_initial_x = x;
-    	home_startup_phase = 0;
+    	home_startup = 0;
     }
 
-    if (detect_ball(sensor_left, sensor_right, sensor_mid, sensor_top, opp_detected)==1){
-        home_startup_phase = 1;
+    if (home_count >= HOME_TIMEOUT){
+        home_startup = 1;
         return SEARCH;
     }
     else if (arm_position == 0.0 && x - home_initial_x >= HOME_AWAY_DISTANCE){
-        home_startup_phase = 1;
+        home_startup = 1;
         return SEARCH;
     }
     else{
         home_count ++;
-       return HOME;
+        return HOME;
     }
 }
 
