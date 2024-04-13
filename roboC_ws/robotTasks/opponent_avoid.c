6edcf9997ab523862f50c577b95154ddf6197c7b
Benjamin Teh <benjamintehjh01@gmail.com>
2024-04-14 02:04:12 +0800

    opp avoid task

---
+++ roboC_ws/robotTasks/opponent_avoid.c
@@ -0,0 +1,33 @@
+#include "../config.h"
+
+float opp_cmd_linX;
+float opp_cmd_angZ;
+
+int opponent_avoid_task(int prev_task, float left_sensor, float right_sensor){
+    static int opponent_avoid_startup;
+    static int opponent_avoid_count;
+
+    if (opponent_avoid_startup == 1){
+        opponent_avoid_count = 0;
+    }
+    opponent_avoid_count ++;
+    opp_cmd_linX = -MAX_SPEED/2.0;
+    if (detect_thing(left_sensor, OPP_DETECT_THRESHOLD)){
+        opp_cmd_angZ = -MAX_TURN/2.0;
+    }
+    else{
+        opp_cmd_angZ = MAX_TURN/2.0;
+    }
+    if (opponent_avoid_count >= OPP_AVOID_TIMER){
+        return prev_task;
+    }
+    return AVOID_OPPONENT;
+}
+
+float get_opp_avoid_linX(){
+    return opp_cmd_linX;
+}
+
+float get_opp_avoid_angZ(){
+    return opp_cmd_angZ;
+}
